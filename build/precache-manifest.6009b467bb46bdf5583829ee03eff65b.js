523e4c703ee1d961623afdbeadd2b2195954f72a
mdiedricks <michael.diedricks@gmail.com>
2021-07-18 22:58:00 +1000

    added build folder to repository

---
+++ build/precache-manifest.6009b467bb46bdf5583829ee03eff65b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9fab8a6f7709ad26c765233fc2b73213",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0b9b1960185f78d985e1",
+    "url": "/static/css/main.f0496591.chunk.css"
+  },
+  {
+    "revision": "cca506bd3ed55c1523b8",
+    "url": "/static/js/2.652e7754.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.652e7754.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0b9b1960185f78d985e1",
+    "url": "/static/js/main.1be8fa49.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  }
+]);
